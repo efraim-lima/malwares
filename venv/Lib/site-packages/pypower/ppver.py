6c40afd575e321f24b82396110a3c21d8839b958
Efraim de Almeida  Lima <efraim.alima@gmail.com>
2023-06-26 08:39:31 -0300

    fix: extinguindo commits

---
+++ venv/Lib/site-packages/pypower/ppver.py
@@ -0,0 +1,19 @@
+# Copyright (c) 1996-2015 PSERC. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+"""PYPOWER version info.
+"""
+
+def ppver(*args):
+    """ Returns PYPOWER version info for current installation.
+
+    @author: Ray Zimmerman (PSERC Cornell)
+    """
+
+    ver = {'Name': 'PYPOWER',
+           'Version': '5.1.16',
+           'Release':  '',
+           'Date': '05-March-2023'}
+
+    return ver
